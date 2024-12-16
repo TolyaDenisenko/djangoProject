d3383bc1b3e4f72809e5e6c3a6e9a4d8454a0835
ANATOLY <anatoliydenisenko44@gmail.com>
2024-12-16 13:27:55 +0200

    homePc

--- base/urls.py
+++ base/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.models import CourseResource, CategoryResource
+from api.models import *
 from tastypie.api import Api
 
 api = Api(api_name='v1')
